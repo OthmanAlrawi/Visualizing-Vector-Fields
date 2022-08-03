f7a83c0572830d0e367b4dc5a248f7b1bc520fc0
OthmanAlrawi <77903464+OthmanAlrawi@users.noreply.github.com>
2022-08-03 15:09:09 +0300

    Update tracer.cs

--- Project/Assets/Scripts/tracer.cs
+++ Project/Assets/Scripts/tracer.cs
@@ -8,12 +8,13 @@ public class tracer : MonoBehaviour
     Rigidbody rigidBody;
     ParticleSystem trailRenderer;
     ParticleSystem.MainModule trailRendererMain;
-    // Controls the speed of the tracer particles or how fast the potential field lines are drawn.
+    
+    //Controls the speed of the tracer particles or how fast the potential field lines are drawn
     public float speed = 0.7f;
     
     private void Start()
     {
-        // Get reference to the trail component attached to the tracer particle.
+        //Get reference to the trail component attached to the tracer particle
         trailRenderer = transform.GetChild(0).gameObject.GetComponent<ParticleSystem>();
         trailRendererMain = trailRenderer.main;
         
@@ -21,22 +22,18 @@ public class tracer : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        // Calculate force at the current position of tracer particle.
+        //Linearly interpolate the color of the tracer's trail according to electric field magnitude
         Vector3 force = testParticle.calculateField(transform.position);
-        
-        // t is a linear interpolation parameter which scales down the force magnitude to a value from 0 to 1 according to maxField/minField.
         float t = force.magnitude / (electricField.maxField - electricField.minField) - (electricField.minField / (electricField.maxField - electricField.minField));
-        
-        // Linearly interpolate the color of the tracer's trail according to t.
         trailRendererMain.startColor = Color.Lerp(Color.blue, Color.red, t); 
         
-        // Sets the velocity direction of the tracer to the field's direction with magnitude to speed. 
+
         rigidBody.velocity = speed * force.normalized;
         
     }
     private void OnCollisionEnter(Collision collision)
     {
-        // If tracer particle collides, 
+        //If tracer particle collides, make it static 
         rigidBody.isKinematic = true;
     }
 }
